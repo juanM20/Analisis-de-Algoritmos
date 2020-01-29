5a6eef2762072a855b81d14c460256affd4d3319
juanM20 <jmtp12poke@gmail.com>
2020-01-29 08:32:59 -0600

    Practica1

--- P1.py
+++ P1.py
@@ -1,7 +1,15 @@
+from time import time
 
+def InsertionSort(A):
+    for i in range(1, len(A)):
+        j = i-1
+        while j >= 0 and A[j] > A[j+1]:
+            aux = A[j]
+            A[j] = A[j+1]
+            A[j+1] = aux
+            j = j-1
 
-def run():
-    print("Hola mundo.")
+    return A
 
 
 
@@ -9,4 +17,12 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
+    A = [2,5,3,7,2312,6,73,6343,732,83,1]
+    
+    t1 = time()
+    InsertionSort(A)
+    t = time() - t1
+
+    print('\nTiempo de ejecucion %0.5f' %t )
+
+    print(A)
