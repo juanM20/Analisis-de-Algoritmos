7d0fb99a1de0bde19ac75cdcbeae4753b3adb30f
juanM20 <jmtp12poke@gmail.com>
2020-02-25 09:40:07 -0600

    Integrales

---
+++ Practica2/Integrales_MC/MC.js
@@ -0,0 +1,20 @@
+
+function Integral_1(x) {
+    return ( ( 1 - (x**2) ) ** (3/2) ) 
+}
+
+function Integral_2(x) {
+    return (Math.E)**( x + x**2 )
+}
+
+function Integral_3(x) {
+    return (1 + x**2) ** 2
+}
+
+function Integral_4(x) {
+    return 1/(Math.cos(x) + 2)
+}
+
+function Integral_5(x) {
+    return Math.log(x)
+}
