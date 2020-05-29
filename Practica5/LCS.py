1e5f7cf286e800524c1794532b9fb98f9220b0b4
juanM20 <jmtp12poke@gmail.com>
2020-05-29 18:14:31 -0500

    Practica5 90%

--- Practica5/LCS.py
+++ Practica5/LCS.py
@@ -22,13 +22,36 @@ def LCS_Length(X,Y):
                 b[i+1,j+1] = 3 
     
     print(c)
+    print('\n')
     print(b)
+
+    return c,b
+
+def Print_LCS(b,X,i,j):
+
+    if i==0 or j==0 :
+        return 
+    
+    if b[i,j] == 1 :
+        Print_LCS(b,X,i-1,j-1)
+        print(X[i-1])
+    
+    elif b[i,j] == 2 :
+        Print_LCS(b,X,i-1,j)
+
+    else:
+        Print_LCS(b,X,i,j-1) 
+
 
 
 X = ['A','B','C','B','D','A','B']
 Y = ['B','D','C','A','B','A']
 
-LCS_Length(X,Y)
+c,b = LCS_Length(X,Y)
+
+print('\n')
+Print_LCS(b,X,len(X),len(Y))
+
 
 
 
